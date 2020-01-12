111905ef522f303381bc79e63148786ae78edb22
Taylor-Jayde Blackstone <t.blackstone@inspyre.tech>
2020-01-11 22:00:02 -0600

    Basic gui restored to function
      * added 'not implemented' popups for features that are not implemented as of yet
      * added files to .gitignore
      * established basic package structure

--- lib/gui/models/windows/start.py
+++ lib/gui/models/windows/start.py
@@ -1,10 +1,12 @@
+
+
 """ Define information for the 'Start New Game' window """
 
 from ..popups import alerts
 
 
 def window():
-    return alerts.not_yet_implemented()
+    pass
 
 
 active = False
--- lib/gui/run.py
+++ lib/gui/run.py
@@ -1,7 +1,29 @@
+class Error(Exception):
+    """ Namespace for monopolpy_companion exceptions """
+    pass
+
+
+class NotYetImplementedError(Error):
+    """
+    Exception raised when a function of the program is called that
+    is currently not implemented, or disabled.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+
 """ Run the Graphical User Interface """
 
-from .models import windows as win
 import logging
+
+import lib.gui.models.popups.alerts as alerts
+from lib.gui.models.windows import app_win
+from lib.gui.models.windows import pm_win as pm_win
 
 formatter = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=formatter)
@@ -9,45 +31,69 @@ log = logging.getLogger('MonopolpyCompanion')
 
 log.debug('Logger set up, and is set to log debug messages')
 
+opts_win_active = False
+opts_win_exist = False
+
 
 def run():
+    global wins_open, opts_win_active, opts_win_exist
+    from lib.gui.models.windows import opts_win_active
+    from lib.gui.models.windows import pm_win_active
+    from lib.gui.models.windows import start_new_active
     while True:
 
-        event, vals = win.app_win.Read(timeout=100)
+        event, vals = app_win.read(timeout=100)
+
         if event is None or event == 'exit_button_main_application':
             log.debug(vals)
             log.info('User exited!')
-            win.app_win.Close()
             log.debug('Main window closed')
             log.debug('Exiting cleanly')
-            exit()
+            break
 
-        if not win.opts_win_active and event == 'options_main_button':
-            win.opts_win_active = True
+        if not opts_win_active and event == 'options_main_button':
+            if not opts_win_exist:
+                opts_win_active = True
+                log.debug('Getting options window')
+            opts_win_active = True
             log.debug('User entered options window!')
-            win.opts_win.Show()
 
-        if win.opts_win_active:
-            opt_event, opt_val = win.opts_win.Read(timeout=100)
+        if opts_win_active:
+            from lib.gui.models.windows import opts_win as opts_win
+            opt_event, opt_val = opts_win.read(timeout=100)
+            opts_win.reappear()
             if opt_event is None or opt_event == 'opts_ok':
                 log.debug('User exited options window')
-                win.opts_win_active = False
-                win.opts_win.Hide()
-            if not win.pm_win_active and opt_event == 'opts_player_man_button':
-                win.pm_win_active = True
+                opts_win_active = False
+                opts_win.disappear()
+
+            if not pm_win_active and opt_event == 'opts_player_man_button':
+                pm_win_active = True
                 log.debug('User entered player management window')
 
-        if win.pm_win_active:
-            pm_event, pm_val = win.pm_win.read()
+        if pm_win_active:
+            pm_event, pm_val = pm_win.read(timeout=0)
             if pm_event is None or pm_event == 'player_man_ok_button':
                 log.debug('User exited the Player Management window')
-                win.pm_active = False
-                win.pm_win.Close()
+                pm_win_active = False
+                pm_win.close()
+
             if pm_event == 'pm_add_button':
                 log.debug('User pressed \'add new\' button')
-                win.pm_add_new_win()
+                try:
+                    raise NotYetImplementedError('The Start New function is not yet implemented')
+                except NotYetImplementedError as error:
+                    log.warning(error)
+                    alerts.not_yet_implemented()
 
-        if not win.start_new_active and event == 'start_new_main_button':
-            # start_event, start_val = win.start_win.Read(timeout=100)
-            # if start_event is None or start_event ==
-            win.start_new_win()
+        if not start_new_active and event == 'start_new_main_button':
+            log.debug('User pressed \'Start new\' in main menu')
+            try:
+                raise NotYetImplementedError('The Start New function is not yet implemented')
+            except NotYetImplementedError as error:
+                log.warning(error)
+                alerts.not_yet_implemented()
+
+    app_win.close()
+
+    exit()
