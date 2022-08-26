f247b4ac79f36507be515d84953057bff862bf80
Taylor B <43686206+tayjaybabee@users.noreply.github.com>
2022-08-26 14:50:44 -0500

    Merge remote-tracking branch 'origin/master'

--- monopolpy_companion/lib/common/run.py
+++ monopolpy_companion/lib/common/run.py
@@ -5,13 +5,77 @@ nyi_active = False
 pm_active = False
 
 
-class ActiveWindows(object):
+class Warden(object):
     def __init__(self):
-        self.__options_win = False
-        self.__player_man_win = False
+        pass
 
 
-class ActivePopUps(object):
+
+
+
+class WindowWarden(Warden):
+    def __init__(self):
+        self.__windows = {}
+
+    @property
+    def windows(self):
+        return self.__windows
+
+    @windows.deleter
+    def windows(self):
+        self.__windows.clear()
+
+    def register(self, name, register_as_active=False):
+        self.__windows[name] = self.Window(name, register_as_active)
+        return self.__windows[name]
+
+    class Window(object):
+        def __init__(self, name, window_object, register_as_active=False):
+            self.__window = window_object
+            if isinstance(name, str):
+                self.__name = name
+            else:
+                raise TypeError(f"Parameter 'name' must be of type 'string' not {type(name)}!")
+
+            self.__active = False
+
+            if register_as_active:
+                if isinstance(register_as_active, bool):
+                    self.__active = True
+                else:
+                    raise TypeError(f"Parameter 'register_as_active' must have a value that is of type 'bool', not "
+                                    f"{type(register_as_active)}")
+
+        @property
+        def name(self):
+            return self.__name
+
+        @property
+        def active(self):
+            return self.__active
+
+        @active.setter
+        def active(self, new):
+            if isinstance(new, bool):
+                self.__active = new
+            else:
+                raise TypeError(f"The new value for 'active' must be of type 'bool' not {type(new)}!")
+
+        @property
+        def window(self):
+            return self.__window
+
+        def __repr__(self):
+            statement = f"WindowWarden.Window:{self.name} ({id(self)} | Active: {self.active} ({id(self.active)}) | " \
+                        f"Window Object: {self.window} ({id(self.window)})"
+            return statement
+
+
+        def toggle_active(self):
+            pass
+
+
+class PopUpWarden(object):
     def __init__(self):
         self.__popups = {}
 
@@ -30,8 +94,8 @@ class ActivePopUps(object):
     @property
     def number_active(self):
         acc = 0
-        for popup in self.__popups:
-            if popup.active:
+        for name in self.__popups.keys():
+            if self.__popups[name].active:
                 acc += 1
 
         return acc
